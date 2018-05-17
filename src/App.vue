896d34f59cd8ba4cd5d5e1562a771e8767d194bf
Julie Nguyen <julie_nguyen@live.fr>
2018-05-17 15:57:30 +0200

    GET all users and fill array

--- src/App.vue
+++ src/App.vue
@@ -11,48 +11,65 @@
       <input class="form-control" type="text" v-model="user.email"/>
     </div>
     <button class="btn btn-primary" @click="submit">Submit</button>
+    <hr>
+    <button class="btn btn-primary" @click="fetchData">Get Data</button>
+    <ul class="list-group">
+      <li class="list-group-item" v-for="u in users"> {{u.username}} - {{u.email}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+  import HelloWorld from './components/HelloWorld'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data() {
-    return {
-      user: {
-        username: '',
-        email: ''
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld
+    },
+    data() {
+      return {
+        user: {
+          username: '',
+          email: ''
+        },
+        users: []
+      }
+    },
+    methods: {
+      submit() {
+        this.$http.post(
+          'https://vuejs-httptest.firebaseio.com/data.json',
+          this.user
+        ).then(response => {
+          console.log(response)
+        }, error => {
+          console.log(error);
+        });
+      },
+      fetchData() {
+        this.$http.get('https://vuejs-httptest.firebaseio.com/data.json')
+          .then(response => {
+            return response.json();
+          }).then(data => {
+            const resultArray = [];
+            for (let key in data) {
+              resultArray.push(data[key]);
+            }
+            this.users = resultArray;
+        });
       }
     }
-  },
-  methods: {
-    submit() {
-      console.log(this.user);
-      this.$http.post(
-        'https://vuejs-httptest.firebaseio.com/data.json',
-        this.user
-      ).then(response => {
-        console.log(response)
-      }, error => {
-        console.log(error);
-      });
-    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
