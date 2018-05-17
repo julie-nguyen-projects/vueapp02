17751a69e2293b8c7eb87b4e7c48d4e8779010aa
Julie Nguyen <julie_nguyen@live.fr>
2018-05-17 14:44:17 +0200

    POST example with Firebase

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,15 @@
   <div id="app">
     <img src="./assets/logo.png">
     <HelloWorld/>
+    <div class="form-group">
+      <label>Username</label>
+      <input class="form-control" type="text" v-model="user.username"/>
+    </div>
+    <div class="form-group">
+      <label>Mail</label>
+      <input class="form-control" type="text" v-model="user.email"/>
+    </div>
+    <button class="btn btn-primary" @click="submit">Submit</button>
   </div>
 </template>
 
@@ -12,6 +21,27 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      user: {
+        username: '',
+        email: ''
+      }
+    }
+  },
+  methods: {
+    submit() {
+      console.log(this.user);
+      this.$http.post(
+        'https://vuejs-httptest.firebaseio.com/data.json',
+        this.user
+      ).then(response => {
+        console.log(response)
+      }, error => {
+        console.log(error);
+      });
+    }
   }
 }
 </script>
